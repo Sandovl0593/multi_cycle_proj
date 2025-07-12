550ced3ade835592246d291f5db6cebf894f6618
Sandovl0593 <adrian.sandoval@utec.edu.pe>
2025-07-12 18:05:25 -0500

    Basys3 hasta ahora, Dios nos cuide

--- src/CLKdivider.v
+++ src/CLKdivider.v
@@ -1,15 +1,21 @@
 module CLKdivider(
-    input in_clk, 
-    input reset_clk,
-    //input stop, // para detener el reloj
+    input wire in_clk, 
+    input wire reset_clk,
     output reg out_clk
 );
-    reg [22:0] counter = 1;
+    reg [17:0] counter = 0;
+    localparam MAX_COUNT = 18'd208333;  // 100e6 / 480 -> 208333
     always @(posedge in_clk or posedge reset_clk) begin
-        if(reset_clk) out_clk <= 0;
-        else begin
+        if(reset_clk) begin
+            out_clk <= 0;
+            counter <= 0;
+        end
+        else if (counter == MAX_COUNT - 1) begin
+            counter <= 0;
+            out_clk <= ~out_clk;
+        end else begin
             counter <= counter + 1;
-            if (counter == 0) out_clk <= ~out_clk;
         end
     end
+
 endmodule
